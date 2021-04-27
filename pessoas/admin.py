b77fa7ac2d53c8b2abadb57450eb8a15bc2959bf
Lucas PIres <luc_50@outlook.com>
2021-04-27 11:45:31 -0300

    Criando um modelo de pessoas

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from pessoas.models import Pessoa
 
-admin.site.register(Pessoa)
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 10 
+
+admin.site.register(Pessoa, ListandoPessoas)
