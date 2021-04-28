d0152c0bcc233a817a0f276b4dd7d669e9cd5395
Lucas PIres <luc_50@outlook.com>
2021-04-28 15:14:25 -0300

    Login e dashboard

--- usuarios/api/views.py
+++ usuarios/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'POST':
@@ -6,16 +7,37 @@ def cadastro(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        print(nome, email, password, password2)
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return render(request, 'usuarios/cadastro.html')
+        if password != password2:
+            print('As passwords não são iguais')
+            return render(request, 'usuarios/cadastro.html')
+        if User.objects.filter(email=email).exists():
+            print('Usiário já cadastrado')
+            return render(request, 'usuarios/cadastro.html')
+        user = User.objects.create_user(username=nome, email=email, password=password)
+        user.save()
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print("Os campos não podem ficar embranco")
+            return render(request, 'usuarios/login.html')
+        print(email, senha)
+        return render(request, 'usuarios/dashboard.html')
     return render(request, 'usuarios/login.html')
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
 
 def logout(request):
     pass
